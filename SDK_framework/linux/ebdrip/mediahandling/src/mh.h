4e55628393ba60efe4d772ffaeb6ab6e262cb972
Vinay Chiniwar <vinay.chiniwar@wipro.com>
2014-08-20 13:27:02 +0900

    mediahandling

---
+++ SDK_framework/linux/ebdrip/mediahandling/src/mh.h
@@ -0,0 +1,31 @@
+typedef int OMenum;
+typedef char* OMname;
+typedef int OMEnum;
+typedef int Uint16;
+typedef int Uint8;
+typedef int Uint32;
+typedef int Sint8;
+typedef int Sint16;
+typedef int Sint32;
+typedef int PAPERCODE;
+typedef int Boolean;
+/*typedef int bool;*/   /*Available in gpssim.h*/
+typedef int ORIENTATION;
+/* Moved to Mhwrapper.h
+void SetMediaType(Uint32 media);
+int GetMediaType();
+OMenum GetPaperSource();
+Uint32 GetPageSize();
+void SetPageSize();
+Uint32 GetCustomPageWidth();
+Uint32 GetCustomPageHeight();
+Uint32 GetPageWidth();
+Uint32 GetPageHeight();
+Uint32 GetOrientation();
+Uint32 GetQualityMode();
+int getMhPageSize();
+int getMhMediaType();
+int getMhPaperSource();
+void SetPaperSource(int tray);
+*/
+

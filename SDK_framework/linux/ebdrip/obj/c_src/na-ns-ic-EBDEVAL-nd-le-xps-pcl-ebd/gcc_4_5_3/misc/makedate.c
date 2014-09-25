4c9a57ecb52c4e3fd99c0a9d41a035ffe7a61bc0
Vinay Chiniwar <vinay.chiniwar@wipro.com>
2014-09-25 11:24:04 +0900

    removed obj

--- SDK_framework/linux/ebdrip/obj/c_src/na-ns-ic-EBDEVAL-nd-le-xps-pcl-ebd/gcc_4_5_3/misc/makedate.c
+++
@@ -1,2 +0,0 @@
-int build_time = 1410848665 ;
-char *build_date = "Tue Sep 16 00:24:25 2014" ;
--- SDK_framework/linux/ebdrip/obj/skinsrc-na-ns-ic-EBDEVAL-nd-epdfouty-eufst5y-pclps3-le-xps-pcl-ebd/pms/src/pms_mem.c
+++
@@ -1,9 +0,0 @@
-#include "pms_mem.h"
-#include <stdio.h>
-
-long memVal = 0;
-long ShowMemVal()
-{
-	printf("Current Memory Usage: %lu bytes\n", memVal);
-	return 0;
-}
